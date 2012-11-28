c9c77d4f1ae7c556c0a846f773c7086e87bb0f80
Francisco <f.thiene@gmail.com>
2012-11-27 23:39:01 -0200

    added file for models

---
+++ node/model.js
@@ -0,0 +1,25 @@
+var ContaSchema = new Schema({
+  id        : ObjectId,
+  conta_id  : ObjectId,
+  nome      : String, 
+  flag      : Number
+});
+
+var FluxoSchema = new Schema({
+  id        : ObjectId,
+  conta_id  : ObjectId,
+  descricao : String, 
+  dt_fluxo  : Date,
+  valor     : Number
+});
+
+var UsuarioSchema = new Schema({
+  id    : ObjectId,
+  nome  : String,
+  email : String, 
+  senha : String
+});
+
+var Conta = mongoose.model('Conta', ContaSchema);
+var Fluxo = mongoose.model('Fluxo', FluxoSchema);
+var Usuario = mongoose.model('Usuario', UsuarioSchema);
