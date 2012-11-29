d0e925b4dd5b3dc794f9c44b4120e51c33c2d956
Maurício Batista <eddloschi@gmail.com>
2012-11-28 23:53:04 -0200

    Authentication

--- node/app.js
+++ node/app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose'),
+    mongoStore = require('connect-mongoose')(express),
     usuarioController = require('./usuarioController'),
     util = require('util');
 
@@ -10,8 +11,23 @@ var app = express();
 app.use(express.logger());
 app.use(express.methodOverride());
 app.use(express.bodyParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.cookieParser());
+app.use(express.session({store: new mongoStore(), secret: '8be63522-150f-43e0-9092-b7341074c007'}));
 
+app.get('/', function (req, res, next) {
+    if (req.session.usuario !== undefined)
+        res.redirect('/principal.html');
+    else
+        next();
+});
+app.get('/principal.html', function (req, res, next) {
+    if (req.session.usuario !== undefined)
+        next();
+    else
+        res.redirect('/');
+});
+
+app.use(express.static(__dirname + '/public'));
 app.post('/login', usuarioController.login);
 
 app.listen(8000);
